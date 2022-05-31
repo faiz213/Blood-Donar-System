920ec9b27ff63c2840b01ba21369c5b7106eb72a
faiz213 <bsef21m554@pucit.edu.pk>
2022-05-31 18:57:58 +0500

    some changes  only one attribute left to add

--- Blood_Donor_System.cpp
+++ Blood_Donor_System.cpp
@@ -1,200 +1,288 @@
-#include <iostream>
+#include<iostream>
 #include <fstream>
 #include <string>
-const int tRecord = 100;
 using namespace std;
-
-fstream file;
-
-struct Data
+int ID;
+struct Student
 {
+    int id;
     string name;
-    string rollno;
-    int donationNum;
-    string Bgroup;
-    string dateofDonation;
+    string bloodGroup;
+    int age;
+    string degree;
+    int semesterNo;
 };
 
-void menu()
+void addStudent()
 {
+    Student student;
+    cin.ignore();
+    cout << "\n\tEnter student name : ";
+    getline(cin, student.name);
+    cout << "\n\tEnter student blood group : ";
+    getline(cin, student.bloodGroup);
+    cout << "\n\tEnter student age : ";
+    cin >> student.age;
+    cout << "\n\tEnter student degree : ";
+    cin >> student.degree;
+    cout << "\n\tEnter student semester number : ";
+    cin >> student.semesterNo;
+    ID++;
 
-    cout << "-----------------FCIT BLOOD DONOR SOCIETY-----------" << endl;
-    cout << "\n\t |------------Main Menu----------------------              |";
-    cout << "\n\t |1. Add Donor Data                                        |";
-    cout << "\n\t |2. Search Donor by name                                  |";
-    cout << "\n\t |3. Search all record of same blood group type            |";
-    cout << "\n\t |4. Update Donor record                                   |";
-    cout << "\n\t.|5. Delete Donor record                                   |";
-    cout << "\n\t |----------------------------------------------           |";
+    ofstream write;
+    write.open("student.txt", ios::app);
+    write << "\n"
+          << ID;
+    write << "\n"
+          << student.name;
+    write << "\n"
+          << student.bloodGroup;
+    write << "\n"
+          << student.age;
+    write << "\n"
+          << student.degree;
+    write << "\n"
+          << student.semesterNo;
+    write.close();
+    write.open("id.txt");
+    write << ID;
+    write.close();
+    cout << "\n\tData save to file"<<endl;
 }
 
-    void addDonor(Data d1[], int &record)
+void print(Student s)
 {
-    file.open("donors_data.txt", ios::out | ios::app | ios::binary);
-    if (file.is_open())
-    {
-        cout << "-----------Add  a Record----------------" << endl;
-        cin.ignore();
-        cout << "Enter Donor name " << record + 1 << endl;
-        getline(cin, d1[record].name);
-        cout << "Enter donor Blood group  " << record + 1 << endl;
-        getline(cin, d1[record].Bgroup);
-        cout << "donation date : " << record + 1 << endl;
-        getline(cin, d1[record].dateofDonation);
-        cout << "Enter Donor number of donations  " << record + 1 << endl;
-        cin >> d1[record].donationNum;
-        file.write(reinterpret_cast<char *>(&d1[record]), sizeof(Data));
+    cout << "\n\t---Stuent Data---";
+    cout << "\n\tID is : " << s.id;
+    cout << "\n\tName is : " << s.name;
+    cout << "\n\tName is : " << s.bloodGroup;
+    cout << "\n\tAge is : " << s.age;
+    cout << "\n\tDegree is : " << s.degree;
+    cout << "\n\tSmester Number is : " << s.semesterNo;
 }
 
-    else
-    {
-        cout << endl;
-        cout << "Eror file opening ";
-    }
-    
-    file.close();
-    
-}
-    
-
-// search donor data by name
-    void searchData(Data d1[], int &record);
+void readData()
 {
-
-    string name;
-    cout << "\n\tEnter donor  name you  want to search : ";
-    cin >> name;
-    Data student;
-    file.open("donors_data.txt", ios::in | ios::binary);
-    if (!read.eof())
+    Student student;
+    ifstream read;
+    read.open("student.txt");
+    while (!read.eof())
     {
-
-        cout << "---------Donor's Data----------" << endl;
-        file.read(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-        cin.ignore();
+        read >> student.id;
+        read.ignore();
         getline(read, student.name);
+        getline(read, student.bloodGroup);
+        read >> student.age;
+        read >> student.degree;
+        read >> student.semesterNo;
+        print(student);
+    }
+    read.close();
+}
+//Checkpoint
+void searchData()
+{
+    cin.ignore();
+    string name;
+    cout << "\n\tEnter student name want to search : ";
+    getline(cin,name);
+    Student student;
+    ifstream read;
+    read.open("student.txt");
+    while (!read.eof())
+    {
+        read >> student.id;
+        read.ignore();
+        getline(read, student.name);
+        getline(read, student.bloodGroup);
+        read >> student.age;
+        read >> student.degree;
+        read >> student.semesterNo;
         if (student.name == name)
         {
-            cout << student;
-            return name;
+            print(student);
         }
     }
 }
-else
+
+void deleteData()
 {
-    cout << endl;
-    cout << "File not opened" << endl;
-}
-    void searchbyBgroup(Data d1[], int &records);
-{
-    int searchbyBgroup;
-    cout << "--Enter the Blood group of donor to see the list of same Blood group donors--" << endl;
-    getline(cin, searchbyBgroup);
-    cout << endl;
-    cout << "---------Data of All Same Blood group type-----------" << endl;
-    cout << searchbyBgroup;
-    cout << endl;
-    file.open("donors_data.txt", ios::in | ios::binary);
-    if (file.is_open())
+    cin.ignore();
+    string name;
+    cout << "\n\tEnter student name want to search : ";
+    getline(cin, name);
+    cout << "\n\tYou want to delete record (y/n) : ";
+    char choice;
+    cin >> choice;
+    if (choice == 'y')
     {
-        for (int i=0; i < records; i++)
+        Student student;
+        ofstream tempFile;
+        tempFile.open("temp.txt");
+        ifstream read;
+        read.open("student.txt");
+        while (!read.eof())
         {
-            file.read(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-            if (d1[i].Bgroup == searchbyBgroup)
+            read >> student.id;
+            read.ignore();
+            getline(read, student.name);
+            getline(read, student.bloodGroup);
+            read >> student.age;
+            read >> student.degree;
+            read >> student.semesterNo;
+            if (student.name != name)
             {
-                searchbyBgroup(d1, i);
+                tempFile << "\n"
+                         << student.id;
+                tempFile << "\n"
+                         << student.name;
+                tempFile << "\n"
+                         << student.bloodGroup;
+                tempFile << "\n"
+                         << student.age;
+                tempFile << "\n"
+                         << student.degree;
+                tempFile << "\n"
+                         << student.semesterNo;
             }
         }
+        read.close();
+        tempFile.close();
+        remove("student.txt");
+        rename("temp.txt", "student.txt");
+        cout << "\n\tData deleted successfuly";
+    }
+    else
+    {
+        cout << "\n\tRecord not deleted";
     }
 }
-    void updateRecord(Data d1[],int &records);
+
+void updateData()
 {
-    string updateRecord;
-    cout<<"Enter the name of donor to update information"<<endl;
-    getline(cin,updateRecord);
-    file.open("donors_data.txt", ios::in | ios::binary | ios::out);
-    if(file.is_open())
+    cin.ignore();
+    string name;
+    cout << "\n\tEnter student name want to search : ";
+    getline(cin, name);
+    cout << "\n\tYou want to update record (y/n) : ";
+    char choice;
+    cin >> choice;
+    if (choice == 'y')
     {
-        for(int i=0;int<records;i++)
+        Student newData;
+        cin.ignore();
+        cout << "\n\tEnter student name : ";
+        getline(cin, newData.name);
+        cout << "\n\tEnter student blood group : ";
+        getline(cin, newData.bloodGroup);
+        cout << "\n\tEnter student age : ";
+        cin >> newData.age;
+        cout << "\n\tEnter student degree : ";
+        cin >> newData.degree;
+        cout << "\n\tEnter student semester number : ";
+        cin >> newData.semesterNo;
+        Student student;
+        ofstream tempFile;
+        tempFile.open("temp.txt");
+        ifstream read;
+        read.open("student.txt");
+        while (!read.eof())
         {
-            file.read(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-            if(d1[i].name == updateRecord)
+            read >> student.id;
+            read.ignore();
+            getline(read, student.name);
+            getline(read, student.bloodGroup);
+            read >> student.age;
+            read >> student.degree;
+            read >> student.semesterNo;
+            if (student.name != name)
             {
-            cout<<"Blood donation of donor updated from "<<d1[i].donationNum <<"to"<<d1[i].donationNum +1<<endl;
-            file.write(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-
+                tempFile << "\n"
+                         << student.id;
+                tempFile << "\n"
+                         << student.name;
+                tempFile << "\n"
+                         << student.bloodGroup;
+                tempFile << "\n"
+                         << student.age;
+                tempFile << "\n"
+                         << student.degree;
+                tempFile << "\n"
+                         << student.semesterNo;
+            }
+            else
+            {
+                tempFile << "\n"
+                         << student.id;
+                tempFile << "\n"
+                         << newData.name;
+                tempFile << "\n"
+                         << newData.bloodGroup;
+                tempFile << "\n"
+                         << newData.age;
+                tempFile << "\n"
+                         << newData.degree;
+                tempFile << "\n"
+                         << newData.semesterNo;
             }
         }
+        read.close();
+        tempFile.close();
+        remove("student.txt");
+        rename("temp.txt", "student.txt");
+        cout << "\n\tData updated successfuly";
+    }
+    else
+    {
+        cout << "\n\tRecord not deleted";
     }
 }
-    void removeRecord(Data d1[],int &records);
-    {
-        string removeRecord;
-        cout<<"Remove donor's record due to medical reasons"<<endl;
-        getline(cin,removeRecord);
-        cout<<endl;
-        file.open("donors_data.txt", ios::in | ios::binary | ios::out);
-        ofstream file;
-        temp.open("temporary_file.txt", ios::binary | ios::out);
-        if (file.is_open() && temporary_file.is_open())
-        {
-            for(int i=0;int<index;i++)
-            {
-                file.read(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-                if (donor[i].rollno!= removeRecord)
-                {
-                    temp.write(reinterpret_cast<char *>(&d1[i]), sizeof(Data));
-                    // to get data from temprory file to original
-                }
-            }
-        }
 
-
-    }
 int main()
 {
-    bool program = 0;
-    Data d1[tRecord];
-    int index = 0;
-    do
+    ifstream read;
+    read.open("id.txt");
+    if (!read.fail())
     {
-        menu();
-        char ch;
-        cout << endl;
-        cout << "Enter the key to funtion :" << endl;
-        cin >> ch;
-        switch (ch)
-        {
-        case '1':
-        {
-            addDonor(d1, index);
-            break;
-        }
-        case '2':
-        {
-            searchData(d1, index);
-            break;
-        }
-         case '3':
-         {
-            searchbyBgroup(d1,index);
-            break;
-         }
-         case '4':
-         {
-           updateRecord(d1,index);
-           break;
-         }
-         case '5':
-         {
-             removeRecord(d1,index);
-             break;
-         }
+        read >> ID;
     }
-    }while(program==0);
+    else
+    {
+        ID = 0;
+    }
+    read.close();
 
+    while (true)
+    {
+       cout << "\t-----------------FCIT BLOOD DONOR SOCIETY-----------" << endl;
+    cout << "\n\t |-------------------Main Menu----------------------|";
+    cout << "\n\t |1. Add Donor Data                                 |";
+    cout << "\n\t |2. Search all  Donors Data                        |";
+    cout << "\n\t |3. Search all record of donors by name            |";
+    cout << "\n\t |4. Delete Donor record                            |";
+    cout << "\n\t |5. Update Donor record                            |";
+    cout << "\n\t |--------------------------------------------------|";
 
-
-
-    return 0;
+        int choice;
+        cout << "\n\tEnter choice : ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            addStudent();
+            break;
+        case 2:
+            readData();
+            break;
+        case 3:
+            searchData();
+            break;
+        case 4:
+            deleteData();
+            break;
+        case 5:
+            updateData();
+            break;
+        }
+    }
 }
